1802db87b59ee6d85196b99c9a49f70f82899503
bitotamas <bito.tamas51@gmail.com>
2021-01-26 12:17:17 +0100

    Oldtimer

---
+++ Oldtimer/oldtimer.js
@@ -0,0 +1,11 @@
+//objektumokat tartalmazó tömb:
+let autok=[
+    {id: 1, nev:"Trabant 601"},
+    {id: 2, nev:"Moszkvics 408"},
+    {id: 3, nev:"DeLorean DMC-12"},
+    {id: 4, nev:"Cadillac Fleetwood Brougham"},
+    {id: 5, nev:"Ferrari 348"},
+    {id: 6, nev:"1970 Ford Mustang Boss 302"},
+]
+//a nem elérhetõ autók azonosítói:
+let nemElerhetoekID=[1,4,6]
